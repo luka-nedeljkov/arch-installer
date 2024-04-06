20df6ab65cd7e6e42479779304953b72148a0797
Luka Nedeljkov <luka.nedeljkov@gmail.com>
2024-04-07 01:18:53 +0200

    Initial commit

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 
+# Source config and fail on error
+source arch-install.conf
+set -e
 
+# Update pacman
+pacman -Syyuu
+
+# Set time
+#timedatelctl set-ntp true
+
+# Zap and partition drives
+read -rp "RAM size: " RAMSIZE
+sgdisk -Z $DRIVE
+sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"esp" $DRIVE
+sgdisk -n 0:0:+${RAMSIZE}G -t 0:8200 -c 0:"swap" $DRIVE
+sgdisk -n 0:0:+48G -t 0:8300 -c 0:"root" $DRIVE
+sgdisk -n 0:0:0 -t 0:8300 -c 0:"home" $DRIVE
+
+# Format partitions
+mkfs.fat -n esp -F32 $ESP
+mkfs.ext4 $ROOT
+mkfs.ext4 $HOME
+mkswap $SWAP
+
+# Mount partitions
+mount $ROOT /mnt
+mount $ESP /mnt/boot --mkdir
+mount $HOME /mnt/home --mkdir
+swapon $SWAP
+
+# Install base system
+pacstrap -K /mnt amd-ucode base base-devel git linux linux-firmware linux-headers neovim openssh zsh
+
+# Congifure /mnt/etc/fstab
+genfstab -U /mnt >>/mnt/etc/fstab
+sed 's/fmask=0022/fmask=0137/' /mnt/etc/fstab
+sed 's/dmask=0022/dmask=0027/' /mnt/etc/fstab
+
+# Prepare chroot
+cp arch-install.conf /mnt/arch-install.conf
+cp chroot-install.sh /mnt/chroot-install.sh
+chmod +x /mnt/chroot-install.sh
+arch-chroot /mnt ./chroot-install.sh
+
+# Unmount and reboot
+umount -R /mnt
+reboot
---
+++ chroot-install.sh
@@ -0,0 +1,64 @@
+# Source config
+source arch-install.conf
+
+# Remount esp
+umount /boot
+mount /boot
+
+# Configure pacman
+sed 's/#Color/Color/' /etc/pacman.conf
+sed 's/#ParallelDownloads=5/ParallelDownloads=5/' /etc/pacman.conf
+sed 's/#ILoveCandy/ILoveCandy/' /etc/pacman.conf
+sed 's/#[multilib]/[multilib]/' /etc/pacman.conf
+sed '/[multilib]/{n; s_#Include = /etc/pacman.d/mirrorlist_Include = /etc/pacman.d/mirrorlist_}' /etc/pacman.conf
+pacman -Syyuu
+
+# Locale
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+
+# Timezone
+ln -sf /usr/share/zoneinfo/Europe/Belgrade /etc/localtime
+hwclock --systohc
+
+# Enable fstrim
+systemctl enable fstrim.timer
+
+# Network
+read hostname
+echo $hostname >/etc/hostname
+pacman -Syu networkmanager
+systemctl enable NetworkManager
+systemctl enable systemd-resolved
+
+# Root password
+passwd root
+
+# Bootloader
+bootctl install
+
+echo "default arch.conf" >/boot/loader/loader.conf
+echo "timeout 0" >>/boot/loader/loader.conf
+echo "editor no" >>/boot/loader/loader.conf
+
+echo "title\tArch Linux" >/boot/loader/entries/arch.conf
+echo "linux\t/vmlinuz-linux" >/boot/loader/entries/arch.conf
+echo "initrd\t/amd-ucode.img" >/boot/loader/entries/arch.conf
+echo "initrd\t/initramfs-linux.img" >/boot/loader/entries/arch.conf
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value $ROOT) rw" >>/boot/loader/entries/arch.conf
+
+# Add user
+useradd -m -G wheel -s /bin/zsh $LUKAUSER
+passwd luka
+chfn luka
+
+echo "\n" | sudo EDITOR="tee -a" visudo
+echo "%wheel ALL=(ALL:ALL) ALL" | sudo EDITOR="tee -a" visudo
+echo "\n" | sudo EDITOR="tee -a" visudo
+echo "Defaults rootpw" | sudo EDITOR="tee -a" visudo
+
+# Remove script and exit chroot
+rm /arch-install.conf
+rm /chroot-install.sh
+exit
