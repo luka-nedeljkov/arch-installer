3360d48f5e0837d620f30c7706ed3c5b5f760b21
Luka Nedeljkov <luka.nedeljkov@gmail.com>
2024-04-16 00:03:00 +0200

    Added neofetch to base install packages

--- post-installer/applications.sh
+++ post-installer/applications.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-pacman -S --noconfirm alacritty bat curl dosfstools e2fsprogs firefox gimp git gptfdisk jdk17-openjdk jdk8-openjdk libreoffice-still man-db man-pages neofetch neovim openssh pacman-contrib smartmontools texinfo tmux tree wget
+pacman -S --noconfirm alacritty bat curl dosfstools e2fsprogs firefox gimp git gptfdisk jdk17-openjdk jdk8-openjdk libreoffice-still man-db man-pages neovim openssh pacman-contrib smartmontools texinfo tmux tree wget
