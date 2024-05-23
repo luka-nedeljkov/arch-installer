14ca578165b2ccac42b1d0d0fdf7f0823e55c792
Luka Nedeljkov <luka.nedeljkov@gmail.com>
2024-05-23 21:13:02 +0200

    Modularized the script more

--- base-installer/base-install.sh
+++ base-installer/base-install.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 
+findroot() {
+	for i in "${partitions[@]}"; do
+		IFS='|' read -ra array <<<"$i"
+		if [[ "${array[4]}" == "/mnt" ]]; then
+			echo "${array[0]}"
+		fi
+	done
+}
+
 # Source config and fail on error
 echo "Loading config"
 source base-install.conf
 set -e
 
-# Detect VM and define drives
-if cat /proc/cpuinfo | grep -q "hypervisor"; then
-	DRIVE=$VM_DRIVE
-	ESP=$VM_ESP
-	SWAP=$VM_SWAP
-	ROOT=$VM_ROOT
-	HOME=$VM_HOME
-else
-	DRIVE=$NVME_DRIVE
-	ESP=$NVME_ESP
-	SWAP=$NVME_SWAP
-	ROOT=$NVME_ROOT
-	HOME=$NVME_HOME
-fi
-
-# Zap and partition drives
+# Partition drives and format partitions
 echo "Setting up partitions"
-sgdisk -Z $DRIVE
-sgdisk -n 0:0:+1G -t 0:ef00 -c 0:"esp" $DRIVE
-sgdisk -n 0:0:+${SWAP_SIZE}G -t 0:8200 -c 0:"swap" $DRIVE
-sgdisk -n 0:0:+48G -t 0:8300 -c 0:"root" $DRIVE
-sgdisk -n 0:0:0 -t 0:8300 -c 0:"home" $DRIVE
-
-# Format partitions
-mkfs.fat -F32 $ESP
-mkfs.ext4 -F $ROOT
-mkfs.ext4 -F $HOME
-mkswap $SWAP
+sgdisk -Z $drive
+for i in "${partitions[@]}"; do
+	IFS='|' read -ra array <<<"$i"
+	sgdisk -n 0:0:${array[1]} -t 0:${array[2]} -c 0:\"${array[3]}\" $drive
+	case ${array[2]} in
+	"ef00" | "ea00")
+		mkfs.fat -F32 ${array[0]}
+		;;
+	"8200")
+		mkswap ${array[0]}
+		;;
+	"8300")
+		mkfs.ext4 -F ${array[0]}
+		;;
+	esac
+done
 
 # Mount partitions
-mount $ROOT /mnt
-mount $ESP /mnt/boot --mkdir
-mount $HOME /mnt/home --mkdir
-swapon $SWAP
+mount $(findroot) /mnt
+for i in "${partitions[@]}"; do
+	IFS='|' read -ra array <<<"$i"
+	if [[ "${array[4]}" != "/mnt" ]]; then
+		if [[ "${array[4]}" = "swap" ]]; then
+			swapon $array[0]
+		elif [[ "${array[4]}" = "/mnt/efi" ]]; then
+			mount $array[0] $array[4] --mkdir -o umask=0077
+		else
+			mount $array[0] $array[4] --mkdir
+		fi
+	fi
+done
 read -rsp "Press enter to continue..."
 #sleep 1s
 clear
 
 # Install base system
-pacstrap -K /mnt amd-ucode base base-devel linux linux-firmware linux-headers
+pacstrap -K /mnt ${cpu}-ucode base base-devel linux linux-firmware
 read -rsp "Press enter to continue..."
 #sleep 1s
 clear
 
 # Congifure /mnt/etc/fstab
 genfstab -U /mnt >>/mnt/etc/fstab
-sed -i 's/fmask=0022/fmask=0137/' /mnt/etc/fstab
-sed -i 's/dmask=0022/dmask=0027/' /mnt/etc/fstab
 
 # Prepare chroot
 cp base-install.conf /mnt/base-install.conf
@@ -64,7 +70,7 @@ arch-chroot /mnt ./chroot-install.sh
 echo "Unmounting all drives"
 rm /mnt/base-install.conf
 rm /mnt/chroot-install.sh
-git clone https://github.com/luka-nedeljkov/arch-scripts /mnt/home/$USER/arch-scripts
+git clone https://github.com/luka-nedeljkov/arch-scripts /mnt/home/$user/arch-scripts
 umount -R /mnt
 
 # Reboot
--- base-installer/chroot-install.sh
+++ base-installer/chroot-install.sh
@@ -1,53 +1,47 @@
+findroot() {
+	for i in "${partitions[@]}"; do
+		IFS='|' read -ra array <<<"$i"
+		if [[ "${array[4]}" = "/mnt" ]]; then
+			echo "${array[0]}"
+		fi
+	done
+}
+
 # Source config
 source ./base-install.conf
 
-# Detect VM and define drives
-if cat /proc/cpuinfo | grep -q "hypervisor"; then
-	DRIVE=$VM_DRIVE
-	ESP=$VM_ESP
-	SWAP=$VM_SWAP
-	ROOT=$VM_ROOT
-	HOME=$VM_HOME
-else
-	DRIVE=$NVME_DRIVE
-	ESP=$NVME_ESP
-	SWAP=$NVME_SWAP
-	ROOT=$NVME_ROOT
-	HOME=$NVME_HOME
-fi
-
-# Remount esp
-umount /boot
-mount /boot
-
 # Configure pacman and install additional packages
 sed -i 's/#Color/Color/' /etc/pacman.conf
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
-sed -i '/ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = ${parralleldownloads}/' /etc/pacman.conf
+if [[ "$ilovecandy" = true ]]; then
+	sed -i '/ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf
+fi
 sed -i 's/#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 sed -i '/\[multilib\]/{n;s_.*_Include = /etc/pacman.d/mirrorlist_}' /etc/pacman.conf
-pacman -Sy --needed --noconfirm neofetch networkmanager zsh
+pacman -Sy --needed --noconfirm $packages
 read -rsp "Press enter to continue..."
 #sleep 1s
 clear
 
 # Locale
-echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+echo "$locale" >>/etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
+echo "LANG=$keymap" >/etc/locale.conf
 
 # Timezone
 echo "Setting timezone"
-ln -sf /usr/share/zoneinfo/Europe/Belgrade /etc/localtime
+ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 # Enable fstrim
-echo "Enabling fstrim.timer service"
-systemctl enable fstrim.timer
+if [[ "$fstrim" = true ]]; then
+	echo "Enabling fstrim.timer service"
+	systemctl enable fstrim.timer
+fi
 
 # Network
 echo "Configuring network"
-echo $HOSTNAME >/etc/hostname
+echo $hostname >/etc/hostname
 systemctl enable NetworkManager
 systemctl enable systemd-resolved
 
@@ -60,24 +54,24 @@ echo "Installing bootloader"
 bootctl install
 
 echo "default arch.conf" >/boot/loader/loader.conf
-echo "timeout 0" >>/boot/loader/loader.conf
+echo "timeout $timeout" >>/boot/loader/loader.conf
 echo "editor no" >>/boot/loader/loader.conf
 
 echo -e "title\tArch Linux" >/boot/loader/entries/arch.conf
 echo -e "linux\t/vmlinuz-linux" >>/boot/loader/entries/arch.conf
-echo -e "initrd\t/amd-ucode.img" >>/boot/loader/entries/arch.conf
+echo -e "initrd\t/${cpu}-ucode.img" >>/boot/loader/entries/arch.conf
 echo -e "initrd\t/initramfs-linux.img" >>/boot/loader/entries/arch.conf
-echo -e "options root=PARTUUID=$(blkid -s PARTUUID -o value $ROOT) rw" >>/boot/loader/entries/arch.conf
+echo -e "options root=PARTUUID=$(blkid -s PARTUUID -o value $(findroot)) rw" >>/boot/loader/entries/arch.conf
 
 # Add user
-echo "Adding user: $USER"
-useradd -m -G wheel -s /bin/zsh $USER
-passwd $USER
-chfn -f $(echo $USER | sed 's/.*/\u&/') $USER
+echo "Adding user: $user"
+useradd -m -G wheel $user
+passwd $user
+chfn -f $(echo $user | sed 's/.*/\u&/') $user
 
 # Sudoers settings
-echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/$USER
-echo "Defaults rootpw" >>/etc/sudoers.d/$USER
+echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/$user
+echo "Defaults rootpw" >>/etc/sudoers.d/$user
 
 # Exit chroot
 exit
