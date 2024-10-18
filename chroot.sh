0c5934ee17ca1088c5f14be16d1aed3653f90d38
Looney <62479506+luka-nedeljkov@users.noreply.github.com>
2024-10-18 02:47:58 +0200

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -20,9 +20,8 @@ fi
 sed -i 's/#\[multilib\]/\[multilib\]/' /etc/pacman.conf
 sed -i '/\[multilib\]/{n;s_.*_Include = /etc/pacman.d/mirrorlist_}' /etc/pacman.conf
 pacman -Sy --needed --noconfirm $packages
-read
-#sleep 1s
-#clear
+sleep 1s
+clear
 
 # Locale
 echo "$locale" >> /etc/locale.gen
@@ -80,8 +79,6 @@ chfn -f $(echo $user | sed 's/.*/\u&/') $user
 # Sudoers settings
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/$user
 echo "Defaults rootpw" >> /etc/sudoers.d/$user
-
-read
 
 # Exit chroot
 exit
